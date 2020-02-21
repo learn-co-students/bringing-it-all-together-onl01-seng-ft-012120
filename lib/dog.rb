2d6f0febc242fbf8e58c7f7a76426a564641317a
Kyle McKenna <mckennakyler@gmail.com>
2020-02-21 06:22:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,6 +33,12 @@ class Dog
     Dog.new(result[0], result[1], result[2])
   end 
   
+  def self.find_by_id
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end   
+  
   def update 
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
